5eff3a1ad71b3155be22558fb4553a02be2fc2d6
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-04-06 00:21:07 +0900

    create_users modify

--- db/migrate/create_users.rb
+++ db/migrate/create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-      t.string :user_image
-      add_column :users, :public, :boolean
+      t.string  :name
+      t.string  :email
+      t.string  :password
+      t.string  :user_image
+      t.boolean :public
 
       t.timestamps
     end
