b02a67be5dc4ce7ea5e04e8f6726fba49778b30e
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-06-20 21:29:23 +0900

    system home_spec.rb reate

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe "Homes", type: :system do
   before do
     driven_by(:rack_test)
+    visit home_path
   end
 
-  
+  it 'should show correct link' do
+    expect(page).to have_link('新規登録', href: user_new_path)
+    expect(page).to have_link('ログイン', href: user_login_path)
+  end
 end
