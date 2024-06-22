c4b5981a1eec543e5c2ca70742761c89c0210cfc
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-06-22 16:55:07 +0900

    requests user new

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
-      expect(response).to have_http_status(200)
+  describe "GET /user/new" do
+    it "should show information correctly" do
+      get user_new_path
+      expect(responce).to have_http_status :ok
+    end
+  end
+
+  describe "POST /user/create" do
+    it 'should create with correct information' do
     end
   end
 end
