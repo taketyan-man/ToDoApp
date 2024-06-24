3ecbeb7f10e8da5959fb3dd7973a92db5f5a3244
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-06-24 19:17:37 +0900

    update requests/user_spec.rb create

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,12 +4,31 @@ RSpec.describe "Users", type: :request do
   describe "GET /user/new" do
     it "should show information correctly" do
       get user_new_path
-      expect(responce).to have_http_status :ok
+      expect(response).to have_http_status :ok
     end
   end
 
   describe "POST /user/create" do
     it 'should create with correct information' do
+      expect {
+        post user_create_path , params: { 
+          public: "true",
+          name: "test",
+          email: "a",
+          password: "1234"
+        }
+      }.to change(User, :count).by 1
     end
+
+    it 'should not create with incorrect information' do
+      expect {
+        post user_create_path, params: {
+          name: "",
+          email: "",
+          password: ""
+        }
+      }.to_not change(User, :count)
+    end
+
   end
 end
