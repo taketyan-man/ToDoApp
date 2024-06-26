12fd33dc83336db0b74b8c7b850ba2230a75d68a
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-06-26 15:04:44 +0900

    system/user_spec udpate describe create

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,4 +4,34 @@ RSpec.describe "Users", type: :system do
   before do
     driven_by(:rack_test)
   end
+
+  describe "#create" do
+    it "should create with correct information" do
+      visit user_new_path
+      
+      fill_in "name",     with: "test"
+      fill_in "email",    with: "a"
+      fill_in "password", with: "1234"
+      choose  "公開"
+
+      click_button "新規登録"
+
+      expect(page).to have_content('ユーザー登録成功しました')
+    end
+
+    it "should not create with incorrect information" do
+      visit user_new_path
+
+      fill_in "name", with: ""
+      fill_in "email", with: ""
+      fill_in "password", with: ""
+      
+      click_button "新規登録"
+
+      expect(page).to have_content('ユーザー名 を入力してください')
+      expect(page).to have_content('メールアドレス を入力してください')
+      expect(page).to have_content('パスワード を入力してください')
+    end
+
+  end
 end
