4401702864d4dc54a437e44797f210c2c6729bce
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-07-01 15:45:44 +0900

    suppoer/helpers udapte

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,8 +1,14 @@
 module Helpers
-  def login_as(user)
-    visit login_path
-    fill_in 'name', with: user.name
-    fill_in 'password', with: user.password
-    click_button 'ログイン'
+  module System
+    def login_as(user)
+      visit login_path
+      fill_in 'name', with: user.name
+      fill_in 'password', with: user.password
+      click_button 'ログイン'
+    end
   end
+end
+
+RSpec.configure do |config|
+  config.include Helpers::System, type: :system
 end
