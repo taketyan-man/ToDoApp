725c89c6b68c455464eb3e5fa7c662706865adf3
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-07-06 17:28:44 +0900

    Users.email add validation uniqueness and format

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,9 @@ class User < ApplicationRecord
   validates :name, presence: {message: "を入力してください"}
   validates :name, length: {maximum: 10, message: "は最大10文字までです"}
   validates :name, uniqueness: {message: "がすでに使われています"}
-  validates :email, presence: {message: "を入力してください"}
+  validates :email, presence: {message: "を入力してください"}, uniqueness: {message: "はすでに登録済みです"}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: {message: "を入力してください"}
   validates :public, inclusion: { in: [true, false] }
 end
